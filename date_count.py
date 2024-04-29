f59092aaeb1f5263c2d9e6820622c55db7a0af6f
Anastasia Kornienko <134001602+jhdwcry@users.noreply.github.com>
2024-04-29 13:07:51 +0300

    Update date_count.py

--- date_count.py
+++ date_count.py
@@ -1,13 +1,15 @@
 import datetime
 
 def get_days_from_today(date):
+    try:
         get_date = datetime.datetime.strptime(date, '%Y-%m-%d')
         current_date = datetime.datetime.today()
 
         date_difference = (current_date - get_date)
 
-        return date_difference
-   
+        return date_difference 
+    except ValueError:
+        return "Invalid date format"
     
 
 print(get_days_from_today("2024-04-20"))
